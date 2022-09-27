a70d9bdc484744aa24a3eb30d22095a086c78409
kloud-sauron <dgudim@gmail.com>
2022-09-27 18:28:59 +0300

    [build] improved whois command

--- commands/whois.ts
+++ commands/whois.ts
@@ -1,7 +1,31 @@
 import { ICommand } from "dkrcommands";
-import { ApplicationCommandOptionType } from "discord.js";
+import { ApplicationCommandOptionType, GuildMember, Role, User } from "discord.js";
 import { dbConnection, tableName } from "..";
 import { error, safeReply } from "discord_bots_common";
+
+function getMinecraftNicknamesById(id: string) {
+    return new Promise<string>(resolve => {
+        dbConnection.query(`SELECT * from ${tableName} WHERE ds_id = 'id_${id}'`,
+            function (err, results) {
+
+                if (err) {
+                    error(err);
+                    return resolve("❌ Sql error ocurred");
+                }
+
+                if (results.length) {
+                    let message = "🖇 Associated minecraft nicknames:";
+                    for (const result of results) {
+                        message += ` ${result.nickname}`;
+                    }
+                    return resolve(message);
+                } else {
+                    return resolve("🚫 No associated minecraft nicknames");
+                }
+
+            });
+    });
+}
 
 export default {
     category: "Administration",
@@ -20,34 +44,26 @@ export default {
             //    ru: "ID, который вы получили из майнкрафта"
             //},
             required: true,
-            type: ApplicationCommandOptionType.User,
+            type: ApplicationCommandOptionType.Mentionable,
         }
     ],
 
-    callback: async ({ interaction }) => {
+    callback: async ({ interaction, guild }) => {
 
         const interaction_nn = interaction!;
-
-        dbConnection.query(`SELECT * from ${tableName} WHERE ds_id = 'id_${interaction_nn.options.getUser("member")?.id || ""}'`,
-            function (err, results) {
-
-                if (err) {
-                    error(err);
-                    safeReply(interaction_nn, "❌ Sql error ocurred", true);
-                    return;
+        const mentionable = interaction_nn.options.getMentionable("member");
+        if (mentionable instanceof User || mentionable instanceof GuildMember) {
+            safeReply(interaction_nn, await getMinecraftNicknamesById(mentionable.id), true);
+        } else if (mentionable instanceof Role) {
+            const members = (await guild?.roles.fetch(mentionable.id))?.members;
+            let str = `--stats of role ${mentionable.name} (${members?.size} users)--`;
+            if (members) {
+                for (const member of members) {
+                    str += `\n${member[1].user.tag}: ${await getMinecraftNicknamesById(member[1].id)}`;
                 }
-
-                if (results.length) {
-                    let message = "🖇 Associated minecraft nicknames:";
-                    for (const result of results) {
-                        message += ` ${result.nickname}`;
-                    }
-                    safeReply(interaction_nn, message, true);
-                } else {
-                    safeReply(interaction_nn, "🚫 No associated minecraft nicknames", true);
-                }
-
-            });
+            }
+            await safeReply(interaction_nn, str, true);
+        }
 
     }
 } as ICommand;
